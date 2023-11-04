6699880e2c699d33ff302b1e7a139704211fd816
xugaoyi <894072666@qq.com>
2023-11-04 23:29:56 +0800

    feat: 完善接口响应处理

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -27,13 +27,13 @@ const httpInterceptor = {
     // 3.添加小程序端请求头标识
     options.header = {
       ...options.header,
-      'source-client': 'miniapp', // 接口要求添加的 source-client 字段
+      'source-client': 'miniapp', // 接口要求添加 source-client 字段
     }
     // 4.添加 token 请求头标识
     const memberStore = useMemberStore()
     const token = memberStore.profile?.token
     if (token) {
-      options.header.Authorization = token // 接口要求添加的 Authorization 字段
+      options.header.Authorization = token // 接口要求添加 Authorization 字段
     }
     // 接口文档：https://apifox.com/apidoc/shared-0e6ee326-d646-41bd-9214-29dbf47648fa/doc-1521513
     console.log(options)
@@ -42,3 +42,63 @@ const httpInterceptor = {
 
 uni.addInterceptor('request', httpInterceptor) // 拦截 request 请求
 uni.addInterceptor('uploadFile', httpInterceptor) //  拦截 uploadFile 文件上传
+
+/**
+ * 请求函数
+ * @param UniApp.RequestOptions
+ * @returns Promise<any>
+ *  1. 返回 Promise 对象
+ *  2. 请求成功 (状态码 2xx)
+ *    2.1 提取核心数据 res.data
+ *    2.2 添加类型，支持泛型
+ *  3. 请求失败
+ *    3.1 401错误 -> 清理用户信息，跳转到登录页
+ *    3.2 其他错误 -> 根据后端错误信息轻提示
+ *    3.3 网络错误 -> 提示用户换网络
+ */
+
+interface Data<T> {
+  code: string
+  msg: string
+  result: T
+}
+export const http = <T>(options: UniApp.RequestOptions) => {
+  // 1. 返回 Promise 对象
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      // 响应成功
+      success(res) {
+        // 状态码 2xx， （axios就是这样设计的）
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 2. 请求成功
+          // 2.1 提取核心数据 res.data
+          // 2.2 添加类型，支持泛型
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          // 3.1 401错误 -> 清理用户信息，跳转到登录页
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          // 3.2 其他错误 -> 根据后端错误信息轻提示
+          uni.showToast({
+            icon: 'none',
+            title: (res.data as Data<T>).msg || '请求错误',
+          })
+          reject(res)
+        }
+      },
+      // 响应失败
+      fail(err) {
+        // 3.3 网络错误 -> 提示用户换网络
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误，换个网络试试',
+        })
+        reject(err)
+      },
+    })
+  })
+}
