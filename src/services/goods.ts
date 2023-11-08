cb23eeb739e040c8b81a0b04ef2a8dfb7ec6bbed
xugaoyi <894072666@qq.com>
2023-11-08 22:58:32 +0800

    feat: 商品详情的页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,12 +1,13 @@
+import type { GoodsResult } from '@/types/goods'
 import { http } from '@/utils/http'
 
 /**
  * 商品详情
- * @param id
+ * @param id 商品id
  * @returns
  */
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
