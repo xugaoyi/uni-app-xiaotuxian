e9b8e4a2d843baf4ffef11c56182be94b8c31e97
xugaoyi <894072666@qq.com>
2023-11-09 22:04:27 +0800

    feat: 设置-实现退出登录

--- src/pagesMember/settings/settings.vue
+++ src/pagesMember/settings/settings.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-//
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+// 退出登录
+const onLogout = () => {
+  uni.showModal({
+    content: '是否退出登录',
+    showCancel: true,
+    success: ({ confirm, cancel }) => {
+      // 确定
+      if (confirm) {
+        // 清理用户信息
+        memberStore.clearProfile()
+        // 返回上一页
+        uni.navigateBack()
+      }
+    },
+  })
+}
 </script>
 
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -21,8 +39,8 @@
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
-      <view class="button">退出登录</view>
+    <view class="action" v-if="memberStore.profile">
+      <view class="button" @tap="onLogout">退出登录</view>
     </view>
   </view>
 </template>
